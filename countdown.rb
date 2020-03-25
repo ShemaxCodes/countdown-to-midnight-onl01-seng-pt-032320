d410a8098d5e80ad7d362a8ad4006afb8f8101b5
Jashema Panapa <jcpanapa@gmail.com>
2020-03-25 18:26:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
-def countdown(integer)
-  integer = 10
-while integer > 0
-  puts "#{integer} SECOND(S)!"
-  integer -= 1
+def countdown
+  x = 10
+while x > 0
+  puts "#{x} SECOND(S)!"
+  x -= 1
 end
 "HAPPY NEW YEAR!"
 end
